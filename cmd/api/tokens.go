61c4fb414b0549925dedf0587d5a2ac69fba639d
vancanhuit <vancanhuit@gmail.com>
2026-07-04 08:59:35 +0700

    fix(api): return 201 Created for authentication token (#4)
    Creating an authentication token creates a new token resource. Match
    the book and return 201 Created instead of 200 OK.

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -59,7 +59,7 @@ func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, 
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"authentication_token": token}, nil)
+	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
